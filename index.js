e2d3db3baef793b00e7afc2b87c9a3527691621c
nickBes <58567842+nickBes@users.noreply.github.com>
2022-02-07 18:57:18 +0200

    changes as per last cr

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
 // we have only one form so we can use querySelector
-let form = document.querySelector('form')
-let matrix;
+const form = document.querySelector('form')
+// An array of coordinates in this format: [[x,y], [x1, y1]...]
+let coords;
 
 form.onchange = async () => {
     const data = new FormData(form)
     // if form doesn't have an input of name file stop
     if (!data.has('file')) return
-    let file = data.get('file')
+    const file = data.get('file')
     if (file instanceof File) {
         const text = await file.text()
-        matrix = parseTextCSV(text)
-        console.log(matrix)
+        coords = parseTextCSV(text)
+        console.log(coords)
     }
 }
 
 function parseTextCSV (text) {
-    if (typeof text === "string") {
-        let parsedRows = text.split('\n') // seperate rows
-                        .map(str => { // parse data into a matrix
-                            // this might set numbers as NaN when string is given
-                            let [x, y] = str.split(',').map(val => parseFloat(val))
-                            // will return 0 for invalid values
-                            if (isInvalidNum(x)) {
-                                return 0
-                            }
+    if (typeof text !== "string") return // used for autocopmletions
+    let parsedRows = text.split(/(?:\n)|(?:\r\n)/) // seperate rows using regex
+                    .map(row => { // parse data into a list of coordinates
+                        // this might set numbers as NaN when string is given
+                        let [x, y] = row.split(',').map(val => parseFloat(val))
+                        // will return 0 for invalid values
+                        if (isInvalidNum(x)) {
+                            return 0
+                        }
 
-                            return [x, y]
-                        })
-                        .filter(val => val) // filter 0 values
-        return parsedRows
-    }
+                        return [x, y]
+                    })
+                    .filter(val => val) // filter 0 values
+    return parsedRows
 }
 
 function isInvalidNum(num) {
